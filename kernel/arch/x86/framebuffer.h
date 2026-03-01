f6c3b0d0dd8ab0260fe6e54273950033b5ae5d75
Alex Manochio <cajm23331@gmail.com>
2026-03-01 22:31:54 +0000

    fix blank issue

--- kernel/arch/x86/framebuffer.h
+++ kernel/arch/x86/framebuffer.h
@@ -19,6 +19,13 @@ void fb_putpixel(uint32_t x, uint32_t y, uint32_t color);
 // Clear screen
 void fb_clear(uint32_t color);
 
+// Compatibility wrappers used by higher-level GUI code
+// Initialize framebuffer with width/height/bpp when address unknown
+void framebuffer_init(int width, int height, int bpp);
+
+// Compatibility clear wrapper
+void framebuffer_clear(uint32_t color);
+
 // Draw a character (optional, if you have a font)
 void fb_putchar(int x, int y, char c, uint32_t fg, uint32_t bg);
 
